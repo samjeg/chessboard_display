d75d46a8dd94c26b3404ffd9be39cf66bfc4fdfe
samjeg <samjegede@outlook.com>
2019-06-30 15:34:47 +0100

    loading js chess pieces

--- js/chessboard_display.js
+++ js/chessboard_display.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-	
+	console.log("Hello chessboard")
+	var chess_piece = document.createElement("DIV");
+	chess_piece.setAttribute("id", "brook1")
+	chess_piece.setAttribute("class", "chesspiece")
+	var chess_icon = document.createTextNode("♜"); 
+	chess_piece.appendChild(chess_icon);
+	document.getElementById("8B").appendChild(chess_piece);
+	// chess_piece.classList.add("chesspiece");                 
+	// document.getElementById("8B").setAttribute("text", "♜")
+	// chess_piece.setAttribute("text", "♜")
+	// chesspiece.appendChild(chess_icon);                         
 });
 
 function robot(){
